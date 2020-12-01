861f4776ca520a3240e068289459da91c86aa795
Guillaume Smet <guillaume.smet@gmail.com>
2020-12-01 16:00:37 +0100

    Initial commit of the GitHub API extension

---
+++ deployment/src/main/java/io/quarkiverse/githubapi/deployment/GithubApiProcessor.java
@@ -0,0 +1,50 @@
+package io.quarkiverse.githubapi.deployment;
+
+import static io.quarkiverse.githubapi.deployment.GitHubApiDotNames.GH_ROOT_OBJECTS;
+import static io.quarkiverse.githubapi.deployment.GitHubApiDotNames.GH_SIMPLE_OBJECTS;
+
+import org.jboss.jandex.DotName;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+
+class GithubApiProcessor {
+
+    private static final String FEATURE = "github-api";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ExtensionSslNativeSupportBuildItem requireSsl() {
+        return new ExtensionSslNativeSupportBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    IndexDependencyBuildItem indexGitHubApiJar() {
+        return new IndexDependencyBuildItem("org.kohsuke", "github-api");
+    }
+
+    @BuildStep
+    void registerForReflection(CombinedIndexBuildItem combinedIndex,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClasses) {
+        for (DotName rootModelObject : GH_ROOT_OBJECTS) {
+            reflectiveClasses.produce(new ReflectiveClassBuildItem(true, true, rootModelObject.toString()));
+
+            reflectiveClasses.produce(new ReflectiveClassBuildItem(true, true,
+                    combinedIndex.getIndex().getAllKnownSubclasses(rootModelObject).stream()
+                            .map(ci -> ci.name().toString())
+                            .toArray(String[]::new)));
+        }
+
+        reflectiveClasses.produce(new ReflectiveClassBuildItem(true, true,
+                GH_SIMPLE_OBJECTS.stream().map(DotName::toString).toArray(String[]::new)));
+    }
+}
