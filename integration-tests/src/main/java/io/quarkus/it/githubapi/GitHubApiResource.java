7d6594a06477a004fc887b45e433ebc5600046cb
Guillaume Smet <guillaume.smet@gmail.com>
2022-01-19 13:32:26 +0100

    Force the use of the JDK 11 HTTP Client
    Okhttp used to be the only alternative in native but now the JDK 11 HTTP
    client is supported and works perfectly in native.
    By refactoring this support, they introduced the usage of a Okhttp
    method that isn't present in the version enforced by the Quarkus BOM so
    for now, we disable Okhttp entirely.
    
    Also added some basic integration tests to test native support. I used
    Quarkus GitHub App to test this until now but let's clean things up.
    
    Fixes #113

---
+++ integration-tests/src/main/java/io/quarkus/it/githubapi/GitHubApiResource.java
@@ -0,0 +1,25 @@
+package io.quarkus.it.githubapi;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+@Path("/test")
+public class GitHubApiResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String extractOuterText() throws IOException {
+        GitHub gitHub = GitHub.connectAnonymously();
+        GHRepository repository = gitHub.getRepository("quarkiverse/quarkus-github-api");
+
+        return repository.getFullName();
+    }
+
+}
